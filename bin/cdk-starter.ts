f836d44618917c0e520315b1a175a14161255448
Borislav Hadzhiev <opmftw@gmail.com>
2021-04-21 17:12:35 +0300

    updated stack instantiation

--- bin/cdk-starter.ts
+++ bin/cdk-starter.ts
@@ -3,4 +3,10 @@ import * as cdk from '@aws-cdk/core';
 import {CdkStarterStack} from '../lib/cdk-starter-stack';
 
 const app = new cdk.App();
-new CdkStarterStack(app, 'CdkStarterStack');
+new CdkStarterStack(app, 'cdk-stack', {
+  stackName: 'cdk-stack',
+  env: {
+    region: process.env.CDK_DEFAULT_REGION,
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+  },
+});
