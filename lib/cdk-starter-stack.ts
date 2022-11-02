0ba53a2391114423662eadab1c16dbe53b24849c
Borislav Hadzhiev <opmftw@gmail.com>
2022-11-02 21:40:08 +0200

    update CDK version

--- lib/cdk-starter-stack.ts
+++ lib/cdk-starter-stack.ts
@@ -10,7 +10,7 @@ export class CdkStarterStack extends cdk.Stack {
     const myFunction = new NodejsFunction(this, 'my-function', {
       memorySize: 1024,
       timeout: cdk.Duration.seconds(5),
-      runtime: lambda.Runtime.NODEJS_14_X,
+      runtime: lambda.Runtime.NODEJS_16_X,
       handler: 'main',
       entry: path.join(__dirname, `/../src/my-lambda/index.ts`),
       bundling: {
